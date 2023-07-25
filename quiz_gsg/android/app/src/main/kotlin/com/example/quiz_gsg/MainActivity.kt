8e81b48ead505b7d2a5dfd3da338f86a878629de
Dana AbuAlkhair <62552887+Dana18-Mohammed@users.noreply.github.com>
2023-07-25 20:23:12 +0300

    Delete quiz_gsg directory

--- quiz_gsg/android/app/src/main/kotlin/com/example/quiz_gsg/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.quiz_gsg
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- quiz_gsg/lib/utilies/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const String quoteLink = "https://api.quotable.io/quotes/random";
-
-const contentTextStyle = TextStyle(
-    fontSize: 24, fontWeight: FontWeight.bold // Adjust the font size as needed
-    );
-//
